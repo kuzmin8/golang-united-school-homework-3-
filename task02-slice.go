7e4714aee7e2bbf9af51e3beaeb12df68dba328f
oleg.kuzmin <oleg1kuzmin@gmail.com>
2022-05-08 13:34:52 +0300

    solve task 2

--- task02-slice.go
+++ task02-slice.go
@@ -1,8 +1,10 @@
 package homework
 
 func reverse(input []int64) (result []int64) {
-	for i := len(input) - 1; i >= 0; i-- {
-		result = append(result, input[i])
+	cinput := make([]int64, len(input))
+	copy(cinput, input)
+	for i := len(cinput) - 1; i >= 0; i-- {
+		result = append(result, cinput[i])
 	}
 	return result
 }
